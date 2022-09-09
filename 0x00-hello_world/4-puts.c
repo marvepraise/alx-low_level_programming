b1e2fba87ab879fe07e35ce4f3b548ce5f4d74f5
marvepraise <marvepraise@gmail.com>
2022-09-08 17:10:53 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
-
+	return (0);
 }
