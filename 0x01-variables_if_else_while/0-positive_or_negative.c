1c7e983a5f3d92c7b13fe5c575dd8d3f1e94911e
marvepraise <marvepraise@gmail.com>
2022-09-09 17:43:29 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /*
-* main: determine if number is positive or negative
+* main - determine if number is positive or negative
 * Description: program will assign a random number to the variable n
 * each time it is executed
 * Return: Always 0 (success)
