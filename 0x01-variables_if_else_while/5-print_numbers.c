c3821c2f76e73b7a1b27efa986db3a6b62a2ae1c
marvepraise <marvepraise@gmail.com>
2022-09-11 06:41:55 -0700

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry
+*
+* return: 0 if successful (always)
+*/
+
+int main(void)
+{
+	char number = '0';
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
