70959f129cf7ed6f6a64e34aefd3f3817c51ff39
marvepraise <marvepraise@gmail.com>
2022-09-13 11:04:59 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print pchar
+ *
+ * Return: always 0.
+ *
+ */
+
+int main(void)
+{
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This programme is for another task on variable if else while project
