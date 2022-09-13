c63c0c62f448400bb3c07fedd3ca89c21197e8bc
marvepraise <marvepraise@gmail.com>
2022-09-13 15:29:51 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ *
+ * Return: Returns nothing
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char item;
+
+	int loop = 0;
+
+	while (loop < 10)
+		{
+
+		for (item = 'a'; item <= 'z'; item++)
+		_putchar(item);
+
+		_putchar('\n');
+
+		loop++;
+		}
+
+}
