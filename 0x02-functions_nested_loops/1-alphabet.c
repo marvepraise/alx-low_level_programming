9275154142888d6dd567f9d67e4814225f29cb51
marvepraise <marvepraise@gmail.com>
2022-09-13 13:47:25 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lower case
+ *
+ * Return: Always 0. 
+ */
+
+void print_alphabet(void)
+{
+
+	char item;
+
+	for (item = 'a'; item <= 'z'; item++)
+		_putchar(item);
+
+		_putchar('\n');
+
+}
