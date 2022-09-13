1d0549d48ef723c38507cc14831ac41abbff5d7d
marvepraise <marvepraise@gmail.com>
2022-09-13 11:27:43 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,22 @@
 /**
  * main - print pchar
  *
- * Return: always 0.
+ * Return: Always 0 (success)
  *
  */
 
 int main(void)
+/** main function */
 {
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("(t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
 
+	return (0);
+}
