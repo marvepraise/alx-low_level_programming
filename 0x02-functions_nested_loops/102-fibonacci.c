60727d265e5f918b37cd6a1b98a0b50584dc9e74
marvepraise <marvepraise@gmail.com>
2022-09-13 17:48:06 -0700

    my commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 = success
+ */
+
+int main(void)
+
+{
+
+	long int val1 = 1;
+
+	long int val2 = 2;
+
+	int count = 0;
+
+	printf("%li, ", val1);
+
+	printf("%li, ", val2);
+
+	while (count < 50)
+
+	{
+
+		int lastvalue = val1 + val2;
+
+		printf("%i, ", lastvalue);
+
+		val1 = val2;
+
+		val2 = lastvalue;
+
+		count++;
+
+	}
+
+
+	return (0);
+
+}
