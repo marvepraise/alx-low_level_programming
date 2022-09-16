d261040db99736a1a33e8b4acbdb5c015af80f33
marvepraise <marvepraise@gmail.com>
2022-09-15 18:58:42 -0700

    my commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers	-	entry
+ * print_numbers - entry
  */
 
 void print_numbers(void)
 {
 
 	char item;
-
 
 	for (item = '0'; item <= '9'; item++)
 		_putchar(item);
