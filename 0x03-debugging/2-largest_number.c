5cf3a43a668a8eec8fc9a8aa093a4edb557886f0
marvepraise <marvepraise@gmail.com>
2022-09-15 17:38:18 -0700

    my commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,15 +16,23 @@ int largest_number(int a, int b, int c)
 {
 
 	int largest;
-	if (a > b && a > c)
-		largest = a;
+	if (year % 4 == 0 || ((year % 100 == 0)))
+	{
+		day++;
+	}
 
-	else if (b > a && b > c)
-		largest = b;
-
-	else if (c > a && c > b)
-		largest = c;
-
-	return (largest);
-
+	printf("day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 366 - day);
+}
+else
+{
+	if (month == 2 && day == 60)
+	{
+		printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	}
+	else
+	{
+		printf("day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
 }
