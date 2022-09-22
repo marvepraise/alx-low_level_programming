b8391e625a1324bade341f6c3247c1298b66ce2a
marvepraise <marvepraise@gmail.com>
2022-09-22 16:28:03 -0700

    my pointer task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - will be void
- *
- * Return: Always 0
- */
-
 int main(void)
 {
 	int n;
@@ -16,11 +10,14 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
-	 p[5] = 98;
-
-	 /* ...so that this prints 98\n */
-	 printf("a[2] = %d\n", a[2]);
-
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
