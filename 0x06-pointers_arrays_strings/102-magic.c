1e45cbb5663aa7ace0d102584c90bdf06cb501f8
marvepraise <marvepraise@gmail.com>
2022-09-22 16:11:11 -0700

    my pointer task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,6 +11,7 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
+
 	a[2] = 1024;
 	p = &n;
 	/*
@@ -21,5 +22,5 @@ int main(void)
 	 /* ...so that this prints 98\n */
 	 printf("a[2] = %d\n", a[2]);
 
-		        return (0);
+	return (0);
 }
