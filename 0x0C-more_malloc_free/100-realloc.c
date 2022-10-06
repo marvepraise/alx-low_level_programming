a1fd26f5d824aa04c8cc759dd985e76463c3c67d
marvepraise <marvepraise@gmail.com>
2022-10-06 06:17:24 -0700

    my assigning

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _realloc - function to reallocate a block of memory
+ *
+ * @ptr: pointer to the memory previously allocated
+ *
+ * @old_size: size of the previous memory
+ *
+ * @new_size: new size in bytes
+ *
+ * Return: pointer to the reallocated memory if successful, NULL, otherwise
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	int *asyn;
+
+	if (new_size == old_size)
+		return (ptr);
+	asyn = realloc(ptr, new_size);
+	if (asyn == NULL)
+		return (NULL);
+
+	return (asyn);
+}
